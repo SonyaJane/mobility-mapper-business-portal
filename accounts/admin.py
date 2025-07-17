23ae2f0a6b1e737de808103163aa93ab114eeb18
SonyaJane <sonya.ridden@gmail.com>
2025-07-17 15:05:50 +0100

    created UserProfile model, registered a signal to automatically create and update the profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    """Admin interface for UserProfile model. Provides:
+    A table view showing both the user and their has_business status.
+    The ability to search by username or email
+    """
+    list_display = ('user', 'has_business')
+    search_fields = ('user__username', 'user__email')
